c38afdf2a39b11aebcfc8578ebc608ddbf84fe01
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-08 12:21:29 +0530

    added serializer for UserRegistration

--- authapi/serializers.py
+++ authapi/serializers.py
@@ -6,4 +6,26 @@ from django.utils.encoding import smart_str, force_bytes, DjangoUnicodeDecodeErr
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.db.auth.tokens import PasswordResetTokenGenerator
 
-from account.utils import Util
+from authapi.utils import Util
+
+
+#serializer for user registration
+class UserRegistrationSerializer(serializers.ModelSerializer):
+	#additional field for user during registration
+	password2 = serializers.CharField(style={'input_type' : 'password'}, write_only=True)
+	class Meta:
+		model = User 
+		fields = ['email', 'name', 'password', 'password2', 'tc']
+		extra_kwargs = {
+			'password' : {'write_only' : True}
+		}
+	#custom validation for password and confirm password
+	def validate(self, attrs):   #data comes from view in attrs in this function
+		password = attrs.get('password')
+		password2 = attrs.get('password2')
+		if password != password2:
+			raise serializers.ValidationError('Password and confirm passwords does not match')
+		return attrs
+
+	def create(self, validate_data):
+		return User.objects.create_user(**validate_data)
