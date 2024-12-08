bc8e4c217204a07e8337c177629d611002812366
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-08 20:47:10 +0530

    added UserLoginSerializer

--- authapi/serializers.py
+++ authapi/serializers.py
@@ -1,10 +1,12 @@
 from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from authapi.models import models
+from authapi.models import User
+
 #for sending a password reset link to email
 from django.utils.encoding import smart_str, force_bytes, DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.db.auth.tokens import PasswordResetTokenGenerator
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 from authapi.utils import Util
 
@@ -29,3 +31,10 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
 	def create(self, validate_data):
 		return User.objects.create_user(**validate_data)
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+	email = serializers.EmailField(max_length=200)
+	class Meta:
+		model = User 
+		fields = ['email', 'password']
