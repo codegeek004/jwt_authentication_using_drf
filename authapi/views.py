30759933ea238561401050ecfce2024c4383d5ec
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-08 19:25:52 +0530

    added UserRegistration class and get_tokens method

--- authapi/views.py
+++ authapi/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from authapi.serializers import UserRegistrationSerializer
+from django.contrib.auth import authenticate
+from authapi.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+#Generate token manually using RefreshToken for a user
+def get_tokens_for_user(user):
+	refresh = RefreshToken.for_user(user)
+
+	return {
+		'refresh' : str(refresh),
+		'access' : str(refresh.access_token),
+	}
+
+#Define view for user registration
+class UserRegistration(APIView):
+	renderer_classes = [UserRenderer]
+	def post(self, request, format=None):
+		#create a user serializer instance
+		serializer = UserRegistrationSerializer(data=request.data)
+		if serializer.is_valid(raise_exception=True):
+			user = serializer.save()
+			token = get_tokens_for_user(user)
+			return Response({'token':token, 'msg':'Registration successful'}, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
