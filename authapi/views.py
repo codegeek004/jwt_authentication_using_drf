56e0955e48e3170432a5292477aebd63216acb11
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-08 20:56:29 +0530

    added UserLoginView

--- authapi/views.py
+++ authapi/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from authapi.serializers import UserRegistrationSerializer
+from authapi.serializers import UserRegistrationSerializer, UserLoginSerializer
 from django.contrib.auth import authenticate
 from authapi.renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -28,4 +28,25 @@ class UserRegistration(APIView):
 			token = get_tokens_for_user(user)
 			return Response({'token':token, 'msg':'Registration successful'}, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogin(APIView):
+	renderer_classes = [UserRenderer]
+	def post(self, request, format=None):
+		serializer = UserLoginSerializer(data=request.data)
+		if serializer.is_valid(raise_exception=True):
+			email = serializer.data.get('email')
+			password = serializer.data.get('password')
+			user = authenticate(email=email, password=password)
+			if user is None:
+				token = get_tokens_for_user(user)
+				return Response({'token':token, 'msg':'Registration successful'}, status=status.HTTP_200_OK)
+			else:
+				return Response({'errors': {'non_field_errors' : ['Email or Password is not valid']}}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+
+
 
