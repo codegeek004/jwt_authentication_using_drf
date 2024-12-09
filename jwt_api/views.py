878f40e3aed8da9a620e58675a39560195c0af6c
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-09 22:51:37 +0530

    added LoginView Class

--- jwt_api/views.py
+++ jwt_api/views.py
@@ -5,6 +5,10 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
+from jwt_api.serializers import RegisterSerializer, LoginSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 
 
@@ -16,3 +20,18 @@ class RegisterView(APIView):
             return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginView(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(username=serializer.validated_data['username'],
+                                password=serializer.validated_data['password'])
+            if user is not None:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                        'access' : str(refresh.access_token),
+                        'refresh' : str(refresh)
+                    })
+            return Response({"error" : "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
