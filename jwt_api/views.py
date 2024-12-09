d198806f292a1b2cc0b31317a0bc16449d9157f6
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-09 15:51:34 +0530

    user registration view

--- jwt_api/views.py
+++ jwt_api/views.py
@@ -3,6 +3,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.serializers import 
+
+#from docs
+from rest_framework_simplejwt.serializers import TokenObtainPairViewSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from rest_framework_simplejwt.tokens import RefreshTokens
+
+class MyTokenObtainPairSerializer(TokenObtainPairViewSerializer):
+	@classmethod
+	def get_token(cls, user):
+		token = super().get_token(user)
+
+		#Add custom claims
+		token['claims'] = user.name
+		return token
 
 class Home(APIView):
 	authentication_classes = [JWTAuthentication]
@@ -11,3 +27,11 @@ class Home(APIView):
 	def get(self, request):
 		content = {'message' : 'Hello World'}
 		return Response(content)
+
+def get_tokens_for_user(user):
+	refresh = RefreshToken.for_user(user)
+
+	return {
+		'refresh' : str(refresh),
+		'access' : str(refresh.access_token),
+	}
