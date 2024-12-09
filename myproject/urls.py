6b3abfde3bdf5ea52f1a054516a48fba0532e38e
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-09 15:09:15 +0530

    urls for token pair

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, , TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenRefreshView.as_view(), name='token_obtain_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh_view'),
     path('', include('jwt_api.urls')),
 ]
