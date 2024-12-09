e3b32595bde7eaa086e21ae46ff7caf51d8a164b
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-09 13:14:54 +0530

    deleted

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,7 +2,7 @@
 URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenVerifyView
+from rest_framework_simplejwt.views import TokenObtainPairView, , TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user', include('authapi.urls')),
+    path('api/token/', TokenRefreshView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh_view'),
+    path('', include('jwt_api.urls')),
 ]
