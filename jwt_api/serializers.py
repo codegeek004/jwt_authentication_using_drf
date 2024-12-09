a799fff997645213b9264a2a8e769d92bb4d078c
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-09 22:34:36 +0530

    added RegisterSerializer with ModelSerializer and LoginSerializer with Serializer class

--- jwt_api/serializers.py
+++ jwt_api/serializers.py
@@ -1,27 +1,21 @@
-# from django.contrib.auth.models import User
-# from rest_framework_simplejwt.tokens import RefreshToken
-# from rest_framework import serializers
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# # Serializer for Register
-# class RegisterSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['username', 'password', 'email']
-#         extra_kwargs = {'password': {'write_only': True}}
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+        extra_kwargs = {'password': {'write_only': True}}
 
-#     def create(self, validated_data):
-#         user = User.objects.create_user(
-#             username=validated_data['username'],
-#             password=validated_data['password'],
-#             email=validated_data['email']
-#         )
-#         return user
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email']
+        )
+        return user
 
-# class login_serializer(serializers.Serializer):
-# 	username=serializers.CharField()
-# 	password=serializers.CharField()
-
-
-
-
+class LoginSerializer(serializers.Serializer):
+	username = serializers.CharField()
+	password = serializers.CharField()
+	
