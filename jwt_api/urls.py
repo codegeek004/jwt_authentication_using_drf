50547365f4786dcae91b2cada47f81007e1fbbf2
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-09 23:10:20 +0530

    added url for login and protected view

--- jwt_api/urls.py
+++ jwt_api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from jwt_api.views import RegisterView
+from jwt_api.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-	path('auth/api/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/api/refresh', TokenRefreshView.as_view(), name='token_refresh_view'),
-	path('register/', RegisterView.as_view(), name="user-registration"),
+	path('register/', RegisterView.as_view(), name="register"),
+	path('login/', LoginView.as_view(), name="login"),
+	path('protected/', ProtectedView.as_view(), name="protected"),
 
 ]
