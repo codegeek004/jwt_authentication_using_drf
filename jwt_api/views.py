271d724a5635650729d239ceb2ef8044b3f77b49
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-12-10 18:45:09 +0530

    Added saml acs view

--- jwt_api/views.py
+++ jwt_api/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views import View
+from onelogin.saml2.auth import OneLogin_Saml2_Auth
+from saml_settings
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from rest_framework import status
@@ -13,55 +16,55 @@ class HomeView(APIView):
     def get(self, request):
         return Response({"message" : "Welcome to the JWT API"})
 
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class RegisterView(APIView):
+#     def post(self, request):
+#         serializer = RegisterSerializer(data=request.data)
+#         if serializer.is_valid():
+#             user = serializer.save()
+#             return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class LoginView(APIView):
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            user = authenticate(username=serializer.validated_data['username'],
-                                password=serializer.validated_data['password'])
-            if user is not None:
-                refresh = RefreshToken.for_user(user)
-                return Response({
-                        'access' : str(refresh.access_token),
-                        'refresh' : str(refresh)
-                    })
-            return Response({"error" : "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class LoginView(APIView):
+#     def post(self, request):
+#         serializer = LoginSerializer(data=request.data)
+#         if serializer.is_valid():
+#             user = authenticate(username=serializer.validated_data['username'],
+#                                 password=serializer.validated_data['password'])
+#             if user is not None:
+#                 refresh = RefreshToken.for_user(user)
+#                 return Response({
+#                         'access' : str(refresh.access_token),
+#                         'refresh' : str(refresh)
+#                     })
+#             return Response({"error" : "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class SamlLoginView(APIView):
-    def get(self, request):
-        #this is where saml triggered you at login page
-        return Response({"message" : "Redirect to SAML Identity Provider for authentication"})
+# class SamlLoginView(APIView):
+#     def get(self, request):
+#         #this is where saml triggered you at login page
+#         return Response({"message" : "Redirect to SAML Identity Provider for authentication"})
 
-class SamlACSView(APIView):
-    def post(self, request):
-        user_data = {
-            'username' : request.data.get('username'),
-            'first_name' : request.data.get('first_name'),
-            'last_name' : request.data.get('last_name')
-        }
-        user, created = User.objects.get_or_create(username=user_data['username'])
-               
-        user.first_name = user_data['first_name']
-        user.last_name = user_data['last_name']
-        user.save()
+# class SamlACSView(APIView):
+#     def post(self, request):
+#         user_data = {
+#             'username' : request.data.get('username'),
+#             'first_name' : request.data.get('first_name'),
+#             'last_name' : request.data.get('last_name')
+#         }
+#         user, created = User.objects.get_or_create(username=user_data['username'])
 
-        #Generate teh jwt token for this
-        refresh = RefreshToken.for_user(user)
-        access_token = str(refresh.access_token)
+#         user.first_name = user_data['first_name']
+#         user.last_name = user_data['last_name']
+#         user.save()
 
-        return Response({
-                'access' : access_token,
-                'refresh' : str(refresh)
-            })
+#         #Generate teh jwt token for this
+#         refresh = RefreshToken.for_user(user)
+#         access_token = str(refresh.access_token)
+
+#         return Response({
+#                 'access' : access_token,
+#                 'refresh' : str(refresh)
+#             })
 
 class ProtectedView(APIView):
     permission_classes = [IsAuthenticated]
