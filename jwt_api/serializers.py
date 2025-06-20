66285ce80e593ebd1968966893cebde590fb4d0b
codegeek004 <yashvaishnav1404@gmail.com>
2025-06-20 21:47:14 +0530

    linked serializer with new cutom user model

--- jwt_api/serializers.py
+++ jwt_api/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth.models import User
+from jwt_api.models import CustomUser
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = CustomUser.objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
             email=validated_data['email']
